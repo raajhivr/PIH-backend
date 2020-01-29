f6b6a89a3a1b93e08e4cfa8b567168c3083cf50d
Thamaraiselvan <818589@cognizant.com>
2020-01-29 20:57:30 +0530

    working code

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,11 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+CORS_ORIGIN_ALLOW_ALL=True
 # Application definition
 
 INSTALLED_APPS = [
     'webapp',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -48,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'mysite.urls'
--- webapp/views.py
+++ webapp/views.py
@@ -5,6 +5,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from webapp.models import ProductInscope
 from django.db.models import Q
+import json
 # product = ProductInscope.objects.all()
 product_list=[]
 product_column = ["nam_prod","bdt","cas_no","spec_id","material_no"]
@@ -12,26 +13,20 @@ last=''
 @csrf_exempt
 def index(requests):
     global last
-    if requests.method=="POST":           
+    if requests.method=="POST": 
         items=[]
-        data = requests.POST
-        if last != data.get('search_key',None):
-            product = ProductInscope.objects.filter(Q(nam_prod__contains=data.get('search_key',None).lower())|
-                                                Q(bdt__contains=data.get('search_key',None).lower())|
-                                                Q(cas_no__contains=data.get('search_key',None).lower())|
-                                                Q(spec_id__contains=data.get('search_key',None).lower())|
-                                                Q(material_no__contains=data.get('search_key',None).lower()))
+        data = requests.body.decode('utf-8')
+        data1=json.loads(data)
+        search = data1.get("SearchData",None)
+        if last != search:
+            product_list=[]
+            product = ProductInscope.objects.filter(Q(nam_prod__startswith=search.lower())|
+                                                Q(bdt__startswith=search.lower()))
             for row in product:
                 namprod={"name":row.nam_prod,"type":"Namprod"}
                 product_list.append(namprod)
                 bdt={"name":row.bdt,"type":"BDT"}
                 product_list.append(bdt)
-                casno = {"name":row.cas_no,"type":"CAS"}
-                product_list.append(casno)
-                specid = {"name":row.spec_id,"type":"specid"}
-                product_list.append(specid)
-                material = {"name":row.material_no,"type":"material"}
-                product_list.append(material)
         # for row in cursor:
         #     val=row[0]
         #     if data.get('search_key').lower() in str(val).lower():
@@ -39,9 +34,9 @@ def index(requests):
         #                 "type":"Namprod"}
         #         items.append(rdata)
         # parameter = config.DATABASES
-        last=data.get('search_key',None)
+        last=search
         res=sorted(product_list, key=lambda k: k['type']) 
-        return JsonResponse(res,safe=False)
+        return JsonResponse(res,content_type="application/json",safe=False)
         # return JsonResponse(product_list)
     elif requests.method=="GET":
         print("mmmm",requests.body)
