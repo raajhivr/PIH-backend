c1709ce45b92a538bf9b8a9594b99069ec3d3e64
Manickam <809918@cognizant.com>
2020-01-30 15:54:35 +0530

    added second level filter

--- webapp/urls.py
+++ webapp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('products', views.all_products, name='all_products'),
+    path('selectedProducts',views.selected_products, name='selected_products')
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -11,31 +11,55 @@ import re
 
 product_column = ["nam_prod","bdt","cas_no","spec_id","material_no"]
 df_product = read_frame(ProductInscope.objects.all(),fieldnames=product_column)
-product_list=[]
+all_product_list=[]
+searched_product_list=[]
 last=''
 
 @csrf_exempt
-def index(requests):
+def all_products(requests):
     global last
-    global product_list
+    global all_product_list
     if requests.method=="POST": 
         data = requests.body.decode('utf-8')
         data_json=json.loads(data)
         search = data_json.get("SearchData",None).strip()
         if last != search:
-            product_list=[]
+            all_product_list=[]
             rex=re.compile(r"(^{})".format(search),re.I)
             for column in product_column:
                 edit_df=df_product[df_product[column].str.contains(rex)]
                 column_value=edit_df[column].unique()
                 for item in column_value:
                     out_dict={"name":item,"type":column}
-                    product_list.append(out_dict)
+                    all_product_list.append(out_dict)
                 
         last=search
-        res=sorted(product_list, key=lambda k: k['type']) 
+        res=sorted(all_product_list, key=lambda k: k['type']) 
         return JsonResponse(res,content_type="application/json",safe=False)
         # return JsonResponse(product_list)
-    elif requests.method=="GET":
-        print("mmmm",requests.body)
-        return HttpResponse("get function called")
+
+@csrf_exempt
+def selected_products(requests):
+    if requests.method=="POST":
+        searched_product_list=[]
+        product_column = ["nam_prod","bdt","cas_no","spec_id","material_no"]
+        data = requests.body.decode('utf-8')
+        data_json=json.loads(data)
+        filter_df=df_product.copy()
+        column_add=[]
+        for item in data_json:
+            search_value = item.get("name")
+            search_column = item.get("type")
+            column_add.append(search_column)
+            filter_df=filter_df[filter_df[search_column]==search_value]
+
+        for remove in column_add:
+             product_column.remove(remove)  
+        
+        for column in product_column:
+            value_list=filter_df[column].unique()
+            for item in value_list:
+                out_dict={"name":item,"type":column}
+                searched_product_list.append(out_dict)
+
+        return JsonResponse(searched_product_list,content_type="application/json",safe=False)
