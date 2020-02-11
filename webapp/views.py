794272f9ce6dbc060cb270b76c94118a6ebefe09
Manickam <809918@cognizant.com>
2020-02-11 19:20:01 +0530

    added few changes

--- webapp/views.py
+++ webapp/views.py
@@ -29,9 +29,9 @@ def all_products(requests):
     try:
         if requests.method=="POST":
             try: 
-                all_products_product_column=["SPEC-ID","NAM PROD","MATERIAL NUMBER","BDT","CAS NUMBER"]
+                all_products_column=[["SPEC-ID","SPEC*"],["NAM PROD","NAM*"],["MATERIAL NUMBER","MAT*"],["BDT","BDT*"],["CAS NUMBER","CAS*"]]
                 selected_categories=["BDT","NAM","CAS","MAT","SPEC"]
-                matched_categories = [["BDT","BDT"],["NAM","NAM PROD"],["MAT","MATERIAL NUMBER"],["CAS","CAS NUMBER"],["SPEC","SPEC-ID"]]
+                matched_categories = [["BDT","BDT","BDT*"],["NAM","NAM PROD","NAM*"],["MAT","MATERIAL NUMBER","MAT*"],["CAS","CAS NUMBER","CAS*"],["SPEC","SPEC-ID","SPEC*"]]
                 data=''
                 data_json=''
                 search=''
@@ -52,7 +52,7 @@ def all_products(requests):
                     print("typecat",search_key)
                     print("typeval",search_value)
                     all_product_list=[]
-                    for item,match in matched_categories:
+                    for item,match,key in matched_categories:
                         if search_key.upper()==item or search.upper()==item:
                             if len(search_value)>0:
                                 rex=re.compile(r"(^{})".format(search_value),re.I)
@@ -61,7 +61,7 @@ def all_products(requests):
                             else:
                                 column_value=df_product_combine[match].unique()                         
                             for value in column_value:
-                                out_dict={"name":str(value).strip(),"type":match}
+                                out_dict={"name":str(value).strip(),"type":match,"key":key}
                                 all_product_list.append(out_dict)
                             break
                     print("new cat",res)
@@ -71,11 +71,11 @@ def all_products(requests):
                     print("all_products",data_json)
                     all_product_list=[]
                     rex=re.compile(r"(^{})".format(search),re.I)
-                    for column in all_products_product_column:
+                    for column,key in all_products_column:
                         edit_df=df_product_combine[df_product_combine[column].astype(str).str.contains(rex,na=False)]
                         column_value=edit_df[column].unique()
                         for item in column_value:
-                            out_dict={"name":str(item).strip(),"type":column}
+                            out_dict={"name":str(item).strip(),"type":column,"key":key}
                             all_product_list.append(out_dict)  
                     print("len of all_product_list",len(all_product_list))
                     print(all_product_list[0:5])
@@ -96,7 +96,7 @@ def selected_products(requests):
                 data=''
                 data_json=''
                 column_add=[]
-                all_product_column=["SPEC-ID","NAM PROD","MATERIAL NUMBER","BDT","CAS NUMBER"]
+                all_product_column=all_products_column=[["SPEC-ID","SPEC*"],["NAM PROD","NAM*"],["MATERIAL NUMBER","MAT*"],["BDT","BDT*"],["CAS NUMBER","CAS*"]]
                 data = requests.body.decode('utf-8')
                 data_json=json.loads(data)
                 print("selectedproducts",data_json)
@@ -111,10 +111,10 @@ def selected_products(requests):
                 for remove in column_add:
                     if remove in all_product_column:
                         all_product_column.remove(remove) 
-                for column in all_product_column:
+                for column,key in all_product_column:
                     value_list=filter_df[column].unique()
                     for item in value_list:
-                        out_dict={"name":str(item).strip(),"type":column}
+                        out_dict={"name":str(item).strip(),"type":column,"key":key}
                         searched_product_list.append(out_dict)
                 print("len of selectedproducts",len(searched_product_list))
                 print(searched_product_list)
